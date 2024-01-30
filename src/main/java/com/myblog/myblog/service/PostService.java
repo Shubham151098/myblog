79d9eb932adca462e3b3515212b2d3b7149dad6a
Shubham Vishwakarma <vishwakarmashubh2@gmail.com>
2024-01-30 17:40:29 +0530

    127: create save Post Feature Created

---
+++ src/main/java/com/myblog/myblog/service/PostService.java
@@ -0,0 +1,8 @@
+package com.myblog.myblog.service;
+
+import com.myblog.myblog.payload.PostDto;
+
+public interface PostService {
+
+    PostDto createPost(PostDto postDto);
+}
---
+++ src/main/java/com/myblog/myblog/service/impl/PostServiceImpl.java
@@ -0,0 +1,35 @@
+package com.myblog.myblog.service.impl;
+
+import com.myblog.myblog.entity.Post;
+import com.myblog.myblog.payload.PostDto;
+import com.myblog.myblog.repository.PostRepository;
+import com.myblog.myblog.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PostServiceImpl implements PostService {
+
+
+    private PostRepository postRepository;
+
+    public PostServiceImpl(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    @Override
+    public PostDto createPost(PostDto postDto) {
+        Post post = new Post();
+        post.setTitle(postDto.getTitle());
+        post.setDescription(postDto.getDescription());
+        post.setContent(postDto.getContent());
+        Post savePost = postRepository.save(post);
+
+        PostDto dto = new PostDto();
+        dto.setTitle(savePost.getTitle());
+        dto.setDescription(postDto.getDescription());
+        dto.setContent(postDto.getContent());
+
+        return dto;
+    }
+}
